89efb29aaa21f85413f95dda425fc7b6e6dedbe1
Simon <gtrsimon@gmail.com>
2018-11-11 22:08:19 -0500

    Create event form in ionic

--- src/app/partials/create-event/create-event.component.ts
+++ src/app/partials/create-event/create-event.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-create-event',
     templateUrl: './create-event.component.html',
     styleUrls: ['./create-event.component.scss']
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit {
 
-    constructor(private modalController: ModalController) { }
+    eventForm: FormGroup;
+
+    constructor(private modalController: ModalController, private toastController: ToastController, private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.eventForm = this.formBuilder.group({
+            vendorUsername: new FormControl(),
+            eventDescription: new FormControl(),
+            startDate: new FormControl(),
+            startTime: new FormControl(),
+            endDate: new FormControl(),
+            endTime: new FormControl(),
+            location: new FormControl(),
+        });
+    }
+
+    async presentToast() {
+        const toast = await this.toastController.create({
+            position: 'bottom',
+            color: 'success',
+            mode: 'ios',
+            message: 'Event Created!',
+            duration: 2000
+        });
+        toast.present();
+    }
 
     closeModal() {
+        this.modalController.dismiss();
+    }
+
+    onSubmit() {
+        this.presentToast();
         this.modalController.dismiss();
     }
 }
