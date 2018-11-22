46229f119ac83ae3201df8436a62be0d959f3368
Boredmp <gizmos46@verizon.net>
2018-11-21 19:38:46 -0500

    updated configurations

--- src/app/services/events-resolver.service.ts
+++ src/app/services/events-resolver.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from "@angular/core";
-import { Resolve } from '@angular/router'
-import { EventService } from '../services/event.service'
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { EventService } from '../services/event.service';
 
-//get vendor data
 @Injectable()
 export class EventsResolverService implements Resolve<any> {
     constructor(private eventService : EventService){}
