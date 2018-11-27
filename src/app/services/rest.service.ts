20f74e7588aa2ac884e406529f62da4221ea8d00
Simon <gtrsimon@gmail.com>
2018-11-27 17:12:11 -0500

    Toggle for Cloud Endpoint

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -7,30 +7,51 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class RestService {
 
+	readonly CLOUD_BASE_PATH = 'http://35.185.38.132:8080';
+	useCloudEndpoint = true;
+
 	httpOptions = {
 		headers: new HttpHeaders({
 			'Content-Type': 'application/json'
 		})
 	};
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) {}
 
-	post(url: string, body: any): Observable<any> {
+	determineUrl(originalUrl: string): string {
+		let url = originalUrl;
+
+		if (this.useCloudEndpoint) {
+			url = this.CLOUD_BASE_PATH + originalUrl;
+		}
+
+		return url;
+	}
+
+	post(path: string, body: any): Observable<any> {
+		let url = this.determineUrl(path);
+
 		return this.http.post<any>(url, body, this.httpOptions)
 			.pipe(catchError(this.handleError<any>('post', [])));
 	}
 
-	delete(url: string) : Observable<any> {
+	delete(path: string) : Observable<any> {
+		let url = this.determineUrl(path);
+
 		return this.http.delete<any>(url)
 			.pipe(catchError(this.handleError<any>('delete', [])));
 	}
 	
-	get(url: string): Observable<any> {
+	get(path: string): Observable<any> {
+		let url = this.determineUrl(path);
+
 		return this.http.get<any>(url)
 			.pipe(catchError(this.handleError<any>('get', [])));
 	}
 
-	promiseGet(url: string, subject: BehaviorSubject<IObject>): Promise<any> {
+	promiseGet(path: string, subject: BehaviorSubject<IObject>): Promise<any> {
+		let url = this.determineUrl(path);
+
 		return new Promise((resolve, reject) => {
 			this.get(url).subscribe(response => {
 				if (response > 300) {
@@ -43,7 +64,9 @@ export class RestService {
 		});
 	}
 
-	promisePost(url: string, subject: BehaviorSubject<IObject>, payload: any): Promise<any> {
+	promisePost(path: string, subject: BehaviorSubject<IObject>, payload: any): Promise<any> {
+		let url = this.determineUrl(path);
+
 		return new Promise((resolve, reject) => {
 			this.post(url, payload).subscribe(response => {
 				if (response > 300) {
